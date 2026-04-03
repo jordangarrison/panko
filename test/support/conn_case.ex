f8385e75a175bf29e08722a9f49bc6319e00ba99
Jordan Garrison <jordangarrison@users.noreply.github.com>
2026-04-02 19:05:17 -0500

    test: add auth test helpers and authentication tests
    Add register_user/1 and log_in_user/2 helpers to ConnCase for
    authenticating in tests. Add auth controller test for sign-out and
    auth redirect tests verifying protected routes redirect and public
    share routes remain accessible.

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -2,29 +2,16 @@ defmodule PankoWeb.ConnCase do
   @moduledoc """
   This module defines the test case to be used by
   tests that require setting up a connection.
-
-  Such tests rely on `Phoenix.ConnTest` and also
-  import other functionality to make it easier
-  to build common data structures and query the data layer.
-
-  Finally, if the test case interacts with the database,
-  we enable the SQL sandbox, so changes done to the database
-  are reverted at the end of every test. If you are using
-  PostgreSQL, you can even run database tests asynchronously
-  by setting `use PankoWeb.ConnCase, async: true`, although
-  this option is not recommended for other databases.
   """
 
   use ExUnit.CaseTemplate
 
   using do
     quote do
-      # The default endpoint for testing
       @endpoint PankoWeb.Endpoint
 
       use PankoWeb, :verified_routes
 
-      # Import conveniences for testing with connections
       import Plug.Conn
       import Phoenix.ConnTest
       import PankoWeb.ConnCase
@@ -34,5 +21,43 @@ defmodule PankoWeb.ConnCase do
   setup tags do
     Panko.DataCase.setup_sandbox(tags)
     {:ok, conn: Phoenix.ConnTest.build_conn()}
+  end
+
+  @doc """
+  Creates a registered user and returns the user struct.
+
+  The returned user has `__metadata__.token` set, which is required
+  for session-based authentication with AshAuthentication.
+  """
+  def register_user(attrs \\ %{}) do
+    params =
+      Map.merge(
+        %{
+          email: "user#{System.unique_integer()}@example.com",
+          password: "password123456",
+          password_confirmation: "password123456"
+        },
+        attrs
+      )
+
+    {:ok, user} =
+      Panko.Accounts.User
+      |> Ash.Changeset.for_create(:register_with_password, params)
+      |> Ash.create(authorize?: false)
+
+    user
+  end
+
+  @doc """
+  Logs in a user by putting auth info in the session.
+  Returns the updated conn.
+
+  Uses AshAuthentication.Plug.Helpers.store_in_session/2 which is the same
+  function used by the AuthController on successful login.
+  """
+  def log_in_user(conn, user) do
+    conn
+    |> Phoenix.ConnTest.init_test_session(%{})
+    |> AshAuthentication.Plug.Helpers.store_in_session(user)
   end
 end
